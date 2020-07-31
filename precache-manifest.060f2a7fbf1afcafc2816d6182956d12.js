4ff015d09729b13011408e93554a547a865c271c
kishmat <kishmat17@gmail.com>
2020-07-31 13:56:40 +0545

    Updates

---
+++ precache-manifest.060f2a7fbf1afcafc2816d6182956d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9781f9093291abb90c489f202e7d0a52",
+    "url": "/memetemplate/index.html"
+  },
+  {
+    "revision": "d647f716b44a94ad5675",
+    "url": "/memetemplate/static/css/main.1d6ea9eb.chunk.css"
+  },
+  {
+    "revision": "13ef91910e9384b13b71",
+    "url": "/memetemplate/static/js/2.1f566ad2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memetemplate/static/js/2.1f566ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d647f716b44a94ad5675",
+    "url": "/memetemplate/static/js/main.e31b1a5b.chunk.js"
+  },
+  {
+    "revision": "6bea66f34523327ed88a",
+    "url": "/memetemplate/static/js/runtime-main.3c032d06.js"
+  }
+]);
